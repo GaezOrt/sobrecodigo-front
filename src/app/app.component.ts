b2e90a39a4572eabe8e891bf4f5b2e4053260a32
GaezOrt <gaezort@gmail.com>
2023-01-28 13:34:02 -0300

    agrego usuarios activos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'sobrecodigo-front';
+  constructor(private router: Router) {
+ 
+   }
+
+   ngOnInit() {
+    this.router.navigateByUrl('/home');
+  }
 }
