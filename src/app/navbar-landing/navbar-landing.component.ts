ab12f4fbb06d6f46d8f96c5c5c60fd32624677d4
valentin1609 <valentin160901@gmail.com>
2023-01-30 02:41:01 -0300

    style(navbar): add styles and become responsive

--- src/app/navbar-landing/navbar-landing.component.ts
+++ src/app/navbar-landing/navbar-landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-landing',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarLandingComponent implements OnInit {
 
-  constructor() { }
+  route : string;
+
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
+    this.route = this.router.url;
   }
+  
+  // show : boolean = true;
+  
+  // switch() :void {
+  //   this.show = !this.show;
+  // }
 
 }
