4e7efd2232a180d3f5090a74b0931e6f56ee4c09
Mauro Vicens <blackeagledhs@gmail.com>
2023-01-31 13:43:15 -0300

    feat: se creo pagina de proyectos, se asigno ruta y se creo un json para muestreo de info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
 import { RankingUsersComponent } from './ranking-users/ranking-users.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
@@ -9,7 +10,9 @@ const routes: Routes = [
   { path: 'sign-up', component: SignUpComponent },
   { path: 'ranking-users', component: RankingUsersComponent },
   { path: 'profile-page', component: ProfilePageComponent },
-  {path: 'home', component:LandingComponent}
+  { path: 'project-page', component: ProjectPageComponent},
+  {path: 'home', component:LandingComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
 
 @NgModule({
