f94c0b0d2340189825cd3ddaf00209cb73eea425
valentin1609 <valentin160901@gmail.com>
2023-02-01 02:08:57 -0300

    style(usercard & card list) mejorando los estilos

--- src/app/cards-active-users/cards-active-users.component.ts
+++ src/app/cards-active-users/cards-active-users.component.ts
@@ -9,6 +9,18 @@ export class CardsActiveUsersComponent implements OnInit {
 
   constructor() { }
 
+  users = [
+    {nombre : '@Gaezort', 
+    desafiosCompletados : '345', 
+    cantidadContribuciones : '30'},
+    {nombre : '@valen', 
+    desafiosCompletados : '35', 
+    cantidadContribuciones : '450'},
+    {nombre : '@eagle', 
+    desafiosCompletados : '335', 
+    cantidadContribuciones : '20'}
+  ]
+
   ngOnInit(): void {
   }
 
