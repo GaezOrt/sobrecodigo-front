933f8887b18d7f165f3ea4f424c648a5699149bf
Mauro Vicens <blackeagledhs@gmail.com>
2023-02-21 18:53:09 -0300

    fix: reacondicionando seccion de perfil de empresa, se creo job-card para reutilizar, todavia queda terminar el modelo que esta dentro de company-profile para luego pasarlo a la card correspondiente. Todavia queda terminar el diseño

---
+++ src/app/jobs-card/jobs-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jobs-card',
+  templateUrl: './jobs-card.component.html',
+  styleUrls: ['./jobs-card.component.css']
+})
+export class JobsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
