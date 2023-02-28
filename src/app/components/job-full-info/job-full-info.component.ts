fe1d88328674347f7326021a3b13763afbe8a4f9
Mauro Vicens <blackeagledhs@gmail.com>
2023-02-28 12:12:27 -0300

    fix: agregando modificaciones para job-description terminado, queda terminar job-full-info y darle responsividad

--- src/app/components/job-full-info/job-full-info.component.ts
+++ src/app/components/job-full-info/job-full-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-job-full-info',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobFullInfoComponent implements OnInit {
 
+  @Input() cardInfo:any;
   constructor() { }
 
   ngOnInit(): void {
