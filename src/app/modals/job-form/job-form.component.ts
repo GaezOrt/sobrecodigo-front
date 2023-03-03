a2946526063536eace91b5c02934ca00db010b42
Mauro Vicens <blackeagledhs@gmail.com>
2023-03-03 10:57:50 -0300

    refactor: Se esta creando formulario para alta de ofertas laborales

--- src/app/modals/job-form/job-form.component.ts
+++ src/app/modals/job-form/job-form.component.ts
@@ -1,6 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
+interface Posicion {
+  value: string;
+  viewValue: string;
+}
+
+interface Employee {
+  value: string;
+  viewValue: string;
+}
+
+interface Modality {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-job-form',
@@ -9,7 +23,33 @@ import { FormControl } from '@angular/forms';
 })
 export class JobFormComponent implements OnInit {
 
-  constructor() { }
+  posicion = new FormControl("Trainee");
+  employee = new FormControl("1-10");
+  modality = new FormControl("Presencial");
+
+  employees: Employee[] = [
+    {value: "1-10", viewValue: "1-10"},
+    {value: "11-50", viewValue: "11-50"},
+    {value: "50 o más", viewValue: "50 o más"}
+  ]
+
+  posiciones: Posicion[] = [
+    { value: "Trainee", viewValue: "Trainee" },
+    { value: "Jr", viewValue: "Jr" },
+    { value: "Ssr", viewValue: "Ssr" },
+    { value: "Sr", viewValue: "Sr"}
+  ];
+
+  modalities: Modality[] = [
+    {value: "Presencial", viewValue: "Presencial"},
+    {value: "Hibrido", viewValue: "Hibrido"},
+    {value: "Remoto", viewValue: "Remoto"}
+  ]
+  constructor() {
+    this.posicion.valueChanges.subscribe(s => {
+      console.log(`The selected value is ${s}`);
+    });
+   }
 
   ngOnInit(): void {
   }
