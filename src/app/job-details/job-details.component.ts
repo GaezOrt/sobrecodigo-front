298be490bf89b5f23a22a1a2ba7a5662a8d9bc74
gc796 <collares.gonzalo@gmail.com>
2023-03-24 00:28:14 -0300

    SO36 - Job Details Page

---
+++ src/app/job-details/job-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-details',
+  templateUrl: './job-details.component.html',
+  styleUrls: ['./job-details.component.css']
+})
+export class JobDetailsComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
